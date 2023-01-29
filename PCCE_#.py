1f81795e01b4d1f8aba9e564308697ed227991c7
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-01-30 02:25:08 +0900

    study restart

--- PCCE_#.py
+++
@@ -1,7 +0,0 @@
-answer = 0
-
-for num in range(10, 101, 2):
-    answer += num
-
-
-print(answer)
--- set_.py
+++
@@ -1,8 +0,0 @@
-data = set([1,1,2,3,4,4,5])
-print(data)
-
-data = {1,1,2,3,4,4,5}
-data = (1,2,3,4,5)
-
-print(data)
-print('today is weekend')
