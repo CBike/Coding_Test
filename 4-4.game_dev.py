5ea5107c5ca161f60e505f94ac71cd056d651c32
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-01-31 20:19:11 +0900

    study restart

---
+++ 4-4.game_dev.py
@@ -0,0 +1,20 @@
+"""
+..............................................
+
+"""
+input_data = input().split()
+data_N = input_data[0]
+data_M = input_data[1]
+
+input_data2 = input().split()
+char_s = list(input_data2[0])
+char_d = input_data2[1]
+
+input_data3 = input().split()
+char_s = list(input_data2[0])
+char_d = input_data2[1]
+
+print(data_N)
+print(data_M)
+print(char_s)
+print(char_d)
