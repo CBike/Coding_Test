ed04f4170784ee01c7f117ef15f1b9981c98fb7b
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-02-08 02:50:12 +0900

    study restart

---
+++ 5-1.Stack_example.py
@@ -0,0 +1,18 @@
+"""
+스택에 대한 이해를 돕기위한 예제
+이것이 코딩테스트다 with Python 126p
+"""
+stack = []
+
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+print(stack)
+print(stack[::-1])
